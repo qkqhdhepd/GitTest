4a4a930b5664de18032ed37bba5011fec959b680
inner8820 <inner8820@gmail.com>
2020-07-13 16:16:18 +0900

    1

---
+++ src/kr/or/ddit/dao/asdasd.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.dao;
+
+public class asdasd {
+	public static void main(String[] args) {
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+		System.out.println("asld;kfjas;ldkfj;aslkdfl;asdkj");
+	}
+}
